bda0cbbfdc368ec479e4a88629d08a660b0d94dc
Maderu-CM <clive.moyia@student.moringaschool.com>
2023-09-08 00:14:24 +0300

    Assignment Done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,15 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(elements):
+    if len(elements) == 0:
+        return ""
+    elif len(elements) == 1:
+        return elements[0]
+    elif len(elements) == 2:
+        return f"{elements[0]} and {elements[1]}"
+    else:
+        # Join all elements except the last one with commas, then add "and" before the last element
+        return ", ".join(elements[:-1]) + f", and {elements[-1]}"
+
+# test
+result = oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+print(result)  # Output: "fiddleheads, okra, and kohlrabi"
+
